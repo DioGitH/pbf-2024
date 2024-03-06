ab70c7c5240c107f91657d012e9a8b3215bb5c1d
Maulidio <maulidiobisnis16@gmail.com>
2024-03-06 09:42:00 +0700

    P4: Praktikum 2

--- pertemuan-04/src/components/button.tsx
+++ pertemuan-04/src/components/button.tsx
@@ -1,14 +1,24 @@
-export default function Tombol_1(){
-    function handleClick(){
+export function Tombol_2({isiPesan, namaTombol}:any){
+    return(
+        <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={() => alert(isiPesan)}>
+            {namaTombol}
+        </button>
+    );
+}
+
+export default function Tombol_1() {
+    function handleClick() {
         alert("Tombol telah ditekan!!!");
     }
 
-    function handleMouseOver(){
+    function handleMouseOver() {
         alert("Eits, mau mencet tombol ya?");
     }
 
-    return(
-        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded" 
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
             onClick={handleClick}
             onMouseOver={handleMouseOver}>
             Ini tombol
