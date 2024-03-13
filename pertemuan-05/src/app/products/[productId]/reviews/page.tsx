7d5a430280f7092c6aee73c5ce17ecdc44cb59be
Maulidio <maulidiobisnis16@gmail.com>
2024-03-13 10:42:43 +0700

    P5: Praktikum 3

---
+++ pertemuan-05/src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+    return (
+        <div>
+            <h1>Review List</h1>
+            <h2>R 1</h2>
+            <h2>R 2</h2>
+            <h2>R 3</h2>
+        </div>
+    )
+}
