a92d230c5707c94213c1f0913016a02f5135d080
Maulidio <maulidiobisnis16@gmail.com>
2024-03-13 09:29:29 +0700

    P5:  Praktikum 1

---
+++ pertemuan-05/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+export default function Profile(){
+    return(
+        <>
+            <h1>Biodata Diri</h1>
+            <ul>
+                <li><h3>Nama: Maulidio Farhan Rizkullah</h3></li>
+                <li><h3>NIM: 2141720041</h3></li>
+                <li><h3>Jurusan: Teknologi Informasi</h3></li>
+                <li><h3>Prodi: D4-Teknik Informatika</h3></li>
+            </ul>
+        </>
+    )
+}
